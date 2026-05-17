2cd997d80d257a39153a6a7ef31bfef7550aaeb6
dinar <dinarsanjaya68@gmail.com>
2026-05-17 15:51:34 +0700

    Update lokasi ke Villa Danau Perungseah dan template WhatsApp

--- src/components/section/detail-info/index.jsx
+++ src/components/section/detail-info/index.jsx
@@ -27,7 +27,7 @@ export default function DetailInfo() {
         <div className="text-center pb-4">
           <div className="mb-2">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.0!2d106.9339224!3d-6.9292253!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68499392fd9211%3A0x7560b4ee773e99c5!2sBidan%20Ika%20Kartika!5e0!3m2!1sid!2sid!4v1716000000000!5m2!1sid!2sid"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.0!2d106.9314172!3d-6.8983629!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6849c694b19093%3A0x43f7fb646e6377e4!2sVILLA%20DANAU!5e0!3m2!1sid!2sid!4v1716000000000!5m2!1sid!2sid"
               style={{
                 border: 0,
                 width: '100%',
@@ -40,7 +40,7 @@ export default function DetailInfo() {
           </div>
           <a
             className="text-center underline"
-            href="https://maps.app.goo.gl/X4u3TaUeiEmXZWtFA"
+            href="https://maps.app.goo.gl/qioubuX9ShXy7XfR9"
             target="_blank"
             rel="noopener noreferrer"
           >
